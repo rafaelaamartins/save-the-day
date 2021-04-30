c00999601bc6041709586bc937ccededf74f481a
rafaelaamartins <95rafamartins@gmail.com>
2021-04-30 13:43:31 +0000

    change api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.BASEURL || 'http://localhost:3333',
+  baseURL: `https://save-the-day-backend.herokuapp.com/`
 })
 
 export default api;

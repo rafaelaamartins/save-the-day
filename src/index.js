80aad4df1d6345583d521c49f0a5faf8f43cdaf2
rafaelaamartins <95rafamartins@gmail.com>
2021-04-30 13:35:36 +0000

    build backend

--- src/index.js
+++ src/index.js
@@ -29,5 +29,5 @@ app.post('/incidents', IncidentController.create);
 app.put('/incidents/:id', IncidentController.edit);
 app.delete('/incidents/:id', IncidentController.delete);
 
-const port = 3333;
+const port = process.env.PORT || 3333;
 app.listen(port, () => console.log(`Server started on port ${port}`));
